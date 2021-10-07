9821aa7cb4c873343d268aaab5abe4d04b823aa3
Maja <mschneider@informatik.uni-leipzig.de>
2021-10-07 17:08:55 +0200

    Fix docstring format.

--- helper/helper.py
+++ helper/helper.py
@@ -1,4 +1,5 @@
-"""Provides helper functions for test classes."""
+"""Provides helper functions for test classes.
+"""
 
 import numpy as np
 
@@ -6,8 +7,17 @@ import numpy as np
 def get_digits(value, number_of_decimals):
     """
     Returns the whole-number digits of value and number_of_decimals values after the comma.
-    :param value: value to apply this method to
-    :param number_of_decimals: number of decimal values to return
-    :return: whole-number digits of value and decimal digits up to number_of_decimals
+
+    Parameters
+    ----------
+    value : float
+        The value to apply this method to.
+    number_of_decimals : int
+        The number of decimal values to return.
+
+    Returns
+    -------
+    int
+        The whole-number digits of 'value' and its decimal digits up to 'number_of_decimals'.
     """
     return np.floor(value * np.power(10, number_of_decimals))
