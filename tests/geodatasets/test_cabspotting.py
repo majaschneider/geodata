3fa93e7922935f6e36409a4be9bc6c2ba08adee0
Maja <mschneider@informatik.uni-leipzig.de>
2022-03-06 11:45:37 +0100

    Fix occupancy detection.

--- tests/geodatasets/test_cabspotting.py
+++ tests/geodatasets/test_cabspotting.py
@@ -1,5 +1,6 @@
 import unittest
 import datetime as dt
+import os
 
 import pandas as pd
 from de4l_geodata.geodata.point_t import PointT
@@ -20,3 +21,4 @@ class TestCabspottingDataset(unittest.TestCase):
             PointT([-122.1343, 37.45667], pd.Timestamp(dt.datetime(2008, 6, 10, 9, 1, 2)), coordinates_unit='degrees'),
             dataset.__getitem__(0).to_degrees()[0]
         )
+        self.assertEqual([1, 2], dataset.data_frame_per_route['stops'].iloc[0])
